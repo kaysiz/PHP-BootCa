0e521ec5f991e31f1a0a6ba339750a96ec69487e
kaysiz <ksiziva@gmail.com>
2018-09-30 16:27:20 +0200

    day01 ex05

--- Day01/ex05/epur_str.php
+++ Day01/ex05/epur_str.php
@@ -1,7 +1,2 @@
 #!/usr/bin/php
-<?php
-
-    $string = implode(" ",preg_split("/[\s]+/",trim($argv[1])));
-
-    echo $string."\n";
-    
+<?= implode(" ",preg_split("/[\s]+/",trim($argv[1])))."\n";?>
