a9f2cb72c49c35562b435f62839ce3deeb6292c2
kudakwashe siziva <9620622+kaysiz@users.noreply.github.com>
2018-10-01 07:09:19 +0200

    Day00 ex01

--- Rush00/install.php
+++ Rush00/install.php
@@ -1,29 +1,61 @@
 <?php
-include './database.php';
-$sql2 = "CREATE TABLE IF NOT EXISTS users ("
-. "id int NOT NULL AUTO_INCREMENT,"
-. "name varchar(100),"
-. "gender varchar(50),"
-. "email varchar(100),"
-. "phone varchar(100),"
-. "occupation varchar(100),"
-. "city varchar(150),"
-. "country varchar(150),"
-. "profile_pic varchar(100),"
-. "profile_pic_url varchar(100),"
-. "password varchar(1000),"
-. "status varchar(50),"
-. "confirmation_code varchar(1000),"
-. "fb_id varchar(150),"
-. "PRIMARY KEY (id));";
-try {
-	$conn = new PDO("mysql:host=$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$conn->exec($sql2);
-	echo "Users table created successfully <br>";
-} catch (PDOException $e) {
-	echo "error: " . $sql2 . "<br>" . $e->getMessage();
-}
+    include './database.php';
 
-$conn = null;
+    $user = "CREATE TABLE IF NOT EXISTS users ("
+    . "id int NOT NULL AUTO_INCREMENT,"
+    . "fullName varchar(100),"
+    . "gender varchar(50),"
+    . "email varchar(100),"
+    . "role varchar(100) DEFAULT 'customer',"
+    . "phone varchar(100),"
+    . "address varchar(150),"
+    . "city varchar(50),"
+    . "country varchar(1000),"
+    . "password varchar(1000),"
+    . "dateCreated date DEFAULT NOW(),"
+    . "PRIMARY KEY (id));";
+    try {
+        $conn = new PDO("mysql:host=$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec($user);
+        echo "Users table created successfully <br>";
+    } catch (PDOException $e) {
+        echo "error: " . $user . "<br>" . $e->getMessage();
+    }
+
+
+    $product = "CREATE TABLE IF NOT EXISTS products ("
+    . "id int NOT NULL AUTO_INCREMENT,"
+    . "name varchar(100),"
+    . "category varchar(100),"
+    . "price varchar(100),"
+    . "quantity varchar(100),"
+    . "dateCreated date DEFAULT NOW(),"
+    . "PRIMARY KEY (id));";
+    try {
+        $conn = new PDO("mysql:host=$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec($user);
+        echo "Procucts table created successfully <br>";
+    } catch (PDOException $e) {
+        echo "error: " . $user . "<br>" . $e->getMessage();
+    }
+
+    $orders = "CREATE TABLE IF NOT EXISTS orders ("
+    . "id int NOT NULL AUTO_INCREMENT,"
+    . "orderNumber varchar(100) UNIQUE,"
+    . "productId int NOT NULL,"
+    . "customerId int NOT NULL,"
+    . "status varchar(100),"
+    . "dateCreated date DEFAULT NOW(),"
+    . "PRIMARY KEY (id));";
+    try {
+        $conn = new PDO("mysql:host=$DB_DSN;dbname=$DB_NAME", $DB_USER, $DB_PASSWORD);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->exec($user);
+        echo "Orders table created successfully <br>";
+    } catch (PDOException $e) {
+        echo "error: " . $user . "<br>" . $e->getMessage();
+    }
+    $conn = null;
 ?>
