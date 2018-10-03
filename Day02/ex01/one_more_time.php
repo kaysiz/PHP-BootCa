7f6d37e204fbf2dae9ebfd4be8810b4c5a0b541a
kaysiz <ksiziva@gmail.com>
2018-10-03 18:32:01 +0200

    Day02 ex01

--- Day02/ex01/one_more_time.php
+++ Day02/ex01/one_more_time.php
@@ -1,3 +1,25 @@
 #!/usr/bin/php
 <?php
-    $months = array("janvier"=> 01, "février" => 02, "mars" =>03, "avril" => 04, "mai" => 05, "juin" => 06, "juillet", "aout", "septembre", "octobre", "novembre", "décembre");
+    $months = array(
+        "janvier"=> 1, "février" => 2, "mars" => 3, "avril" => 4, "mai" => 5, "juin" => 6,
+        "juillet" => 7, "aout" => 8, "septembre" => 9, "octobre" => 10, "novembre" => 11, "décembre" => 12
+    );
+
+    if($argc < 2)
+        exit;
+    $my_date = preg_split("/[\s]+/", $argv[1]);
+    if(sizeof($my_date) < 5)
+    {
+        echo "Wrong Format\n";
+        exit;        
+    }
+    unset($my_date[0]);
+    $month = str_pad($months[strtolower($my_date[2])], 2, "0", STR_PAD_LEFT);
+    if($month == 00)
+    {
+        echo "Wrong Format\n";
+        exit;        
+    }
+    date_default_timezone_set("Europe/Paris");
+    echo strtotime($my_date[3]."/".$month."/".$my_date[1].' '.$my_date[4])."\n";
+?>
