741ba3f7990cfbfd905f90d8205d4da194d7173f
kaysiz <ksiziva@gmail.com>
2018-10-09 07:48:51 +0200

    day05

--- Day05/ex10/ex10.sql
+++ Day05/ex10/ex10.sql
@@ -1,1 +1,4 @@
-SELECT title AS `Title`, summary AS `Summary`, prod_year  FROM film WHERE id_genre = 25 ORDER BY prod_year DESC;
+SELECT title AS `Title`, summary AS `Summary`, prod_year  FROM film
+INNER JOIN genre ON genre.id_genre = film.id_genre
+WHERE genre.name = 'erotic'
+ORDER BY prod_year DESC;
